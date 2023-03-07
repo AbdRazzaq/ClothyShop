2151684fedf8b7fbadd5b2ce2c1cdecba5482feb
AbdulRazzaq <abdulrazzaqd16@gmail.com>
2023-03-07 14:23:43 +0530

    First Commit

--- app/src/main/java/com/example/clothyshop/repository/CartRepository.kt
+++ app/src/main/java/com/example/clothyshop/repository/CartRepository.kt
@@ -5,12 +5,11 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.clothyshop.db.ShopDB
 import com.example.clothyshop.model.cart.CartModelClass
-import com.example.clothyshop.model.product.ProductsModelClass
 import com.example.clothyshop.retrofit.ShopApi
 import com.example.clothyshop.utils.NetworkUtils
 import javax.inject.Inject
 
-class CartRepository@Inject constructor(private val shopApi: ShopApi,
+class CartRepository @Inject constructor(private val shopApi: ShopApi,
                                         private val shopDB: ShopDB,
                                         private val applicationContext: Context) {
     private val cartsLiveData = MutableLiveData<CartModelClass>()
